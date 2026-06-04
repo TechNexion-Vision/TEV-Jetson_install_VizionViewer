2f513f4118e6d34d6ce6ab320877c51a7adb4be5
Tifa Chen <tifa.chen@technexion.com>
2026-06-04 13:06:12 +0800

    script: setup 8cam demo desktop launcher only when demo app exist.

--- usr/sbin/install_demo_script.sh
+++ usr/sbin/install_demo_script.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
+TEK_ORIN=$(cat /proc/device-tree/nvidia,dtsfilename | rev | cut -d '/' -f 1 | rev | grep "tek-orin")
+TN316_ORIN=$(cat /proc/device-tree/nvidia,dtsfilename | rev | cut -d '/' -f 1 | rev | grep "vl316")
+DEMO_BIN="/opt/vizionviewer/bin/TechNexion_8_Cam_Demo"
+
+if [[ (-z ${TEK_ORIN} && -z ${TN316_ORIN}) || ! -e ${DEMO_BIN} ]];then
+	logger -t 8cam-demo '==================================='
+	logger -t 8cam-demo 'This board have no 8 camera demo app.'
+	logger -t 8cam-demo '==================================='
+	exit 0
+fi
+
 echo '***********************************' > /dev/ttyTCU0
 echo 'Install Demo Script START!!!' > /dev/ttyTCU0
 echo '***********************************' > /dev/ttyTCU0
-
-TEK_ORIN=$(cat /proc/device-tree/nvidia,dtsfilename | rev | cut -d '/' -f 1 | rev | grep "tek-orin")
-TN316_ORIN=$(cat /proc/device-tree/nvidia,dtsfilename | rev | cut -d '/' -f 1 | rev | grep "vl316")
-
-if [ -z ${TEK_ORIN} ] && [ -z ${TN316_ORIN} ];then
-	echo '===================================' > /dev/ttyTCU0
-	echo 'This board have no 8 camera demo script.' > /dev/ttyTCU0
-	echo '===================================' > /dev/ttyTCU0
-	return 0
-fi
 
 AUTOSSTART_FILE_PATH="/home/ubuntu/.config/autostart/setup_technexion_8_cam_demo.desktop"
 DEMO_SCRIPT='/home/ubuntu/Desktop/TechNexion_8_Cam_Demo.desktop'
